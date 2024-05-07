539e440582f330ee6a3545d68d5eafefbdb78d84
LittleBlueGamer <148423893+LittleBlueGamer427@users.noreply.github.com>
2024-05-07 13:16:58 -0700

    Update motion.js

--- src/resources/blocks/motion.js
+++ src/resources/blocks/motion.js
@@ -26,6 +26,41 @@ function register() {
     }, () => {
         return [`util.target.y`, javascriptGenerator.ORDER_ATOMIC];
     })
+    registerBlock(`${categoryPrefix}dir`, {
+        message0: 'direction',
+        args0: [],
+        output: null,
+        previousStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, () => {
+        return [`util.target.direction`, javascriptGenerator.ORDER_ATOMIC];
+    })
+    registerBlock(`${categoryPrefix}setaxis`, {
+        message0: 'set %1 to %2',
+        args0: [
+            {
+                "type": "field_dropdown",
+                "name: "AXIS",
+                "options": [
+                    [ "x", "${VALUE}, util.target.y" ],
+                    [ "y", "util.target.x, ${VALUE}" ],
+                ]  
+            },
+            {
+                "type": "input_value",
+                "name: "VALUE",
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, () => {
+        const AXIS = block.getFieldValue('AXIS')
+        const VALUE = block.getFieldValue('VALUE')
+        return [`util.target.setXY(${AXIS});`, javascriptGenerator.ORDER_ATOMIC];
+    })
 }
 
 export default register;
